887fb88ca1776d172af774be7e191bdacffe78f8
Mik1810 <92441000+Mik1810@users.noreply.github.com>
2021-10-27 15:27:27 +0200

    Add files via upload

---
+++ CifrariodiCesare.java
@@ -0,0 +1,7 @@
+public class CifrariodiCesare {     //definire input da dare nel prompt e comando che legge l'input
+    public static void main (String[] args) {
+        String a = "c";             //scrivere tutte le lettere dell'alfabeto e le cifre spostate di 3 posizioni nell'alfabeto/elenco di cifre 
+        System.out.println(a);      //far stampare la concatenazione delle lettere e cifre crittografate
+    }
+}                                   //per il futuro: cercare di definire un array dove sono presenti le lettere e le cifre da stampare e un ulteriore array con lett. e cifre spostate in base alla chiave di crittografia
+                                    //per il futuro: poter rendere la chiave variabile in modo tale da definire ogni volta loo spostamento nell'array (in contrasto con l'idea di definire un secondo array con le lett. e cifre spostate)
